9c42e872da54d746e242daedb7ecefbfcdeed6ba
Pete Birkinshaw <pete@binary-ape.org>
2020-04-09 22:16:05 +0100

    Make all but one controller functions private

--- lib/yubikey_otp/controller.ex
+++ lib/yubikey_otp/controller.ex
@@ -16,11 +16,11 @@ defmodule YubikeyOTP.Controller do
     |> verify_response()
   end
 
-  def prepare_api_tasks(request, urls) do
+  defp prepare_api_tasks(request, urls) do
     Enum.map(urls, fn (url) -> Task.async(fn -> HTTP.verify(request, url) end) end)
   end
 
-  def make_concurrent_api_calls(tasks) do
+  defp make_concurrent_api_calls(tasks) do
     tasks
     |> Task.yield_many(@timeout)
     |> Enum.map(
@@ -38,32 +38,33 @@ defmodule YubikeyOTP.Controller do
        )
   end
 
-  def task_success_and_completion(response, task, tasks) do
+  defp task_success_and_completion(response, task, tasks) do
+    Apex.ap response
     if response.status == :ok do
       immediately_kill_other_tasks(task, tasks)
     end
     response
   end
 
-  def immediately_kill_other_tasks(_this_task, all_tasks) do
+  defp immediately_kill_other_tasks(_this_task, all_tasks) do
     all_tasks
     |> Enum.each(fn task -> Task.shutdown(task, :brutal_kill) end)
   end
 
-  def sort_responses(responses) do
+  defp sort_responses(responses) do
     responses
     |> Enum.filter(fn r -> !is_nil(r) end)
     |> Enum.sort_by(&(&1.timestamp))
   end
 
-  def select_primary_response(responses) do
+  defp select_primary_response(responses) do
     case Enum.find(responses, fn r -> r.status == :ok end) do
       %Response{} = response -> response
       _ -> List.first(responses)
     end
   end
 
-  def verify_response(response) do
+  defp verify_response(response) do
     if response.status == :ok do
       {:ok, response.status}
     else
@@ -71,7 +72,7 @@ defmodule YubikeyOTP.Controller do
     end
   end
 
-  def task_failure_response(code, _message) do
+  defp task_failure_response(code, _message) do
     Response.new(
       halted: true,
       otp: "error",
