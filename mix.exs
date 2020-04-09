3065dda2b7bdd194d1b2b44c964fcb36c154c5cd
Pete Birkinshaw <pete@binary-ape.org>
2020-04-09 22:24:30 +0100

    Expanded the Mix configuration for publishing via Hex.pm

--- mix.exs
+++ mix.exs
@@ -5,17 +5,34 @@ defmodule YubikeyOTP.MixProject do
     [
       app: :yubikey_otp,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.7",
+      description: "Elixir client library for validating Yubikey one-time-passwords (OTPs)",
+      package: package(),
+      name: "YubikeyOTP",
+      source_url: "https://github.com/Digital-Identity-Labs/yubikey_otp",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      test_coverage: [
+        tool: ExCoveralls
+      ],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      docs: [
+        main: "readme",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ],
       deps: deps()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
@@ -34,6 +51,15 @@ defmodule YubikeyOTP.MixProject do
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
       {:ex_unit_assert_match, "~> 0.3.0", only: :test},
       {:ex_matchers, "~> 0.1.3", only: :test}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/Digital-Identity-Labs/yubikey_otp"
+      }
     ]
   end
 end
